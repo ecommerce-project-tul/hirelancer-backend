88dc55fb7d835d9dc7ba540ecf7bcf7443cdd2dc
dawid12349 <dawid12349@interia.pl>
2022-04-14 16:54:57 +0200

    add dto package and create UserDto

---
+++ src/dto/UserDto.ts
@@ -0,0 +1,51 @@
+import { IsEmail, IsEnum, IsOptional, IsString } from "class-validator";
+import { EUserRole } from "../enum/EUserRole";
+
+export default class UserDto {
+    @IsEmail()
+    email: string;
+    
+    @IsString()
+    password: string;
+
+    @IsString()
+    firstName: string;
+
+    @IsString()
+    lastName: string;
+
+    @IsEnum(EUserRole)
+    role: EUserRole;
+
+    @IsString()
+    @IsOptional()
+    photo?: string;
+
+    @IsString()
+    @IsOptional()
+    linkedInToken?: string;
+
+    @IsString()
+    @IsOptional()
+    githubLink?: string;
+
+    @IsString()
+    @IsOptional()
+    linkedInLink?: string;
+    
+    @IsString()
+    @IsOptional()
+    description?: string;
+    
+    @IsOptional()
+    reviews?: any[]
+
+    @IsOptional()
+    announcements: any[]
+
+    @IsOptional()
+    offers: any[]
+
+    @IsOptional()
+    userTechnologyStacks: any[]
+ }
