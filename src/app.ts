3018cef88cedca0ee08a641cf940747433d16bde
PicuBicu <championello@gmail.com>
2022-05-07 18:22:13 +0200

    Add cors for all addresses

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ import AuthController from './auth/AuthController';
 import errorMiddleware from './middleware/error-middleware';
 import loggerMiddleware from './middleware/logger-middleware';
 import AnnouncementController from './announcement/AnnouncementController';
+import cors from 'cors';
 
 dotenv.config();
 const app = express()
@@ -16,6 +17,7 @@ const app = express()
 app.set("views", path.join(__dirname, 'views'))
 app.set("view engine", 'ejs')
 
+app.use(cors());
 app.use(logger("dev"))
 app.use(express.json());
 app.use(express.urlencoded({extended: false}))
@@ -27,7 +29,7 @@ app.use("/", new UserController().router);
 app.use("/", new AnnouncementController().router);
 app.use(errorMiddleware)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 appDataSource.initialize()
 .then(() => {
     app.listen(PORT, ()=> console.log(`Server is listening on PORT - ${PORT}`))
